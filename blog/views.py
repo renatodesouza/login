c9e2a1348c7e13faebb3ee5a5eae18efc89ffa4a
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-07-27 10:45:21 -0300

    feat: criação da view PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+    paginate_by = 5
+
+    
